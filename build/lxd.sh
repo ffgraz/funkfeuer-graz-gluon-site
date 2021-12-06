577a0b45be4fedb85677faf260b76ff00039750c
Maciej Krüger <mkg20001@gmail.com>
2021-12-06 21:13:10 +0100

    fix: lxd

--- build/lxd.sh
+++ build/lxd.sh
@@ -2,7 +2,7 @@
 
 lxc launch images:debian/12 -c security.privileged=true gluon
 yes "
-" | lxc exec gluon adduser "$USER"
+" | lxc exec gluon adduser "$(id -un)" --uid "$(id -u)"
 SRC=$(readlink -f ..)
 lxc exec gluon apt update
 yes | lxc exec gluon apt install build-essential python2 libncurses-dev unzip make
