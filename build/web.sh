91e023156cebea0147063e51cdf95a4445e9440a
Maciej Krüger <mkg20001@gmail.com>
2021-12-06 21:24:06 +0100

    fix

--- build/web.sh
+++ build/web.sh
@@ -3,8 +3,8 @@
 
 SELF=$(dirname $(readlink -f "$0"))
 
-markdown "$SELF/../README.md"
-mv "$SELF/README.html" /tmp/index.html
+markdown "$SELF/../../README.md"
+mv "README.html" /tmp/index.html
 
 echo '<style>body { background: #181a1b; color: #e8e6e3; font-family: sans-serif; } a { color: #00a3e0; }</style>' > /storage/ffgraz/www/index.html
 cat /tmp/index.html >> /storage/ffgraz/www/index.html
