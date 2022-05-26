70d38b2e5b3916e52ccc5283663eb749b71b4a2b
Maciej Krüger <mkg20001@gmail.com>
2022-05-26 12:38:37 +0200

    fix up.sh

--- scripts/up.sh
+++ scripts/up.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "$0")/../.." || exit 1
 
 git -C site branch -D master-updates || true
-bash scripts/update-modules.sh
+bash site/scripts/update-modules.sh
 git -C site goto master
 git -C site merge master-updates
 git -C site push
