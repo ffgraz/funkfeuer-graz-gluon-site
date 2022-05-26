f62ebf9fba772c2e0bb8c0f467cbb61cc1f80c83
Maciej Krüger <mkg20001@gmail.com>
2022-05-26 12:38:02 +0200

    add up.sh

---
+++ scripts/up.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd "$(dirname "$0")/../.." || exit 1
+
+git -C site branch -D master-updates || true
+bash scripts/update-modules.sh
+git -C site goto master
+git -C site merge master-updates
+git -C site push
