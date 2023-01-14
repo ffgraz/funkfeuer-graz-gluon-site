e6004252ffd6f002c1b615a4a37d98a1c509409d
5gbr <raczgabor3@gmail.com>
2023-01-14 21:56:46 +0100

    fix update-modules

--- scripts/up.sh
+++ scripts/up.sh
@@ -4,6 +4,6 @@ cd "$(dirname "$0")/../.." || exit 1
 
 git -C site branch -D master-updates || true
 bash site/scripts/update-modules.sh
-git -C site goto master
+git -C site checkout master
 git -C site merge master-updates
 git -C site push
