42fc0faa92580d1a28baf95f90b9c593f6ea30fb
Maciej Krüger <mkg20001@gmail.com>
2023-10-14 03:34:06 +0200

    fix container

--- build/lxd.sh
+++ build/lxd.sh
@@ -5,6 +5,6 @@ yes "
 " | lxc exec gluon -- adduser --uid "$(id -u)" "$(id -un)"
 SRC=$(readlink -f ..)
 lxc exec gluon apt update
-yes | lxc exec gluon apt install build-essential python2 libncurses-dev unzip make git gawk wget python3 python3-distutils rsync qemu-utils
+yes | lxc exec gluon apt install build-essential python3 python3-distutils libncurses-dev unzip make git gawk wget python3 python3-distutils rsync qemu-utils
 yes | lxc exec gluon apt install ca-certificates file git subversion python3 build-essential gawk unzip libncurses5-dev zlib1g-dev libssl-dev libelf-dev wget rsync time qemu-utils ecdsautils lua-check shellcheck
 lxc config device add gluon src disk "source=$SRC" "path=$SRC"
